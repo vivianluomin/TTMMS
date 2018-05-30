6273e2a6e0899ff3bd4ec496f4f5b03c9f237abf
vivianluomin <273104241@qq.com>
2018-05-30 10:36:49 +0800

    修改登录界面

--- src/xupt/se/ttms/view/LoadUI.java
+++ src/xupt/se/ttms/view/LoadUI.java
@@ -1,5 +1,7 @@
 package xupt.se.ttms.view;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
@@ -8,6 +10,7 @@ import java.awt.event.WindowEvent;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
@@ -16,9 +19,9 @@ import xupt.se.ttms.view.tmpl.ImagePanel;
 
 public class LoadUI extends JFrame {
 	
-	private int frmWidth=1024;
-	private int frmHeight=700;
-	protected ImagePanel headPan = new ImagePanel("resource/image/header.jpg");
+	private int frmWidth=500;
+	private int frmHeight=400;
+	protected ImagePanel headPan = new ImagePanel("resource/image/bg.jpg");
 	private JTextField userText;
 	private JPasswordField passwordText;
 
@@ -42,31 +45,41 @@ public class LoadUI extends JFrame {
 	
 	
 	private void init(){
-		headPan.setBounds(0, 0, frmWidth, 80);
-		headPan.setLayout(null);
-		this.add(headPan);
+		
+//		JLayeredPane layerpane = new JLayeredPane();
+//		headPan.setBounds(0, 0, frmWidth, frmHeight);
+//		headPan.setLayout(null);
+//		//this.add(headPan);
+//		layerpane.add(headPan, 0,1);
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 0, frmWidth, frmHeight);
+		panel.setLayout(null);
 		
 		
 		// 创建 JLabel
         JLabel userLabel = new JLabel("User:");
+        userLabel.setFont(new Font(null,Font.PLAIN,16));
         /* 这个方法定义了组件的位置。
          * setBounds(x, y, width, height)
          * x 和 y 指定左上角的新位置，由 width 和 height 指定新的大小。
          */
         userLabel.setBounds(frmWidth/4,frmHeight/3,80,25);
-        this.add(userLabel);
+        panel.add(userLabel);
+        
 
         /* 
          * 创建文本域用于用户输入
          */
          userText = new JTextField(20);
+         
         userText.setBounds(frmWidth/4+100,frmHeight/3,165,25);
-        this.add(userText);
+        panel.add(userText);
 
         // 输入密码的文本域
         JLabel passwordLabel = new JLabel("Password:");
+        passwordLabel.setFont(new Font(null,Font.PLAIN,16));
         passwordLabel.setBounds(frmWidth/4,frmHeight/3+50,80,25);
-        this.add(passwordLabel);
+        panel.add(passwordLabel);
 
         /* 
          *这个类似用于输入的文本域
@@ -74,12 +87,12 @@ public class LoadUI extends JFrame {
          */
         passwordText = new JPasswordField(20);
         passwordText.setBounds(frmWidth/4+100,frmHeight/3+50,165,25);
-        this.add(passwordText);
+        panel.add(passwordText);
         
  
         
         btnManagerLoad.setBounds(frmWidth/4+200,frmHeight/3+100, 120, 25);
-        this.add(btnManagerLoad);
+        panel.add(btnManagerLoad);
         btnManagerLoad.addActionListener(new ActionListener(){
 
 			@Override
@@ -91,7 +104,8 @@ public class LoadUI extends JFrame {
         	
         });
         
-		
+		panel.setBackground(new Color(255, 228, 225));
+		this.setContentPane(panel);
 	}
 	
 	private void onWindowClosing(){
